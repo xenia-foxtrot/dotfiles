dca65ab194d195bf8e4130c4d85695e258c983e5
Izzy <avrisaac555@gmail.com>
2019-04-03 22:26:36 +0200

    Added .config directory to install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+B
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -sv ~/.dotfiles/zshrc ~/.zshrc
+ln -sv ~/.dotfiles/config ~/.config
