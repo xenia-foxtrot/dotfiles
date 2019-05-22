59981c133b49d993195b00364e18cfcda599c49a
Izzy! <izzy@Izzy-Luna.local>
2019-05-22 13:11:49 -0700

    Install script is no longer directory-dependent

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
-cd ~
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-B
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+#!/bin/bash
+
+# Get location of script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+OMZ_HOME=$DIR/.oh-my-zsh
+export ZPLUG_HOME=$DIR/.zplug
+
+# 1. Install oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ${OMZ_HOME}
+
+# 2. Install zplug
+git clone https://github.com/zplug/zplug ${ZPLUG_HOME}
+zsh source ${ZPLUG_HOME}/init.zsh
+
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sv ~/.dotfiles/zshrc ~/.zshrc
-ln -sv ~/.dotfiles/config ~/.config
+ln -sv ${DIR}/zshrc ~/.zshrc
+ln -sv ${DIR}/config ~/.config
