b430831fa47c72b0c6e6d87eee4acfffe6dc1365
Izzy Lancaster <izzylanfox@gmail.com>
2025-04-21 22:55:36 -0700

    Added default formatters for rust and JS/TS

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -268,6 +268,9 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
 			},
 		},
 	},
