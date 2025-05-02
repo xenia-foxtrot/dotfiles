3e27ebc03c52fb9c0329c92d80ec1de2a9bc6f0b
Izzy Lancaster <isaavram@amazon.com>
2025-05-01 17:30:40 -0700

    Add java lsp and configure to work with Bemol

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -27,6 +27,9 @@ return {
 
 			-- Better completion menu than built-in, faster than nvim-cmp
 			"saghen/blink.cmp",
+
+			-- LSP Extensions for jdtls
+			"mfussenegger/nvim-jdtls",
 		},
 		config = function()
 			-- This will run whenever an LSP attaches to a buffer.
@@ -212,9 +215,12 @@ return {
 
 			-- Ensure servers and other tools are installed
 			require("mason-tool-installer").setup({
-				"lua_ls",
-				"stylua",
-				"rust-analyzer",
+				ensure_installed = {
+					"lua_ls",
+					"stylua",
+					"rust-analyzer",
+					"jdtls",
+				},
 			})
 
 			require("mason-lspconfig").setup({
@@ -232,6 +238,39 @@ return {
 							},
 						},
 					}),
+					["jdtls"] = make_server_handler(function(server)
+						-- Bemol is an external dependency that integrates LSP servers with brazil
+						local function bemol()
+							local bemol_dir = vim.fs.find(
+								{ ".bemol" },
+								{ upward = true, type = "directory" }
+							)[1]
+							local ws_folders_lsp = {}
+							if bemol_dir then
+								local file =
+									io.open(bemol_dir .. "/ws_root_folders", "r")
+								if file then
+									for line in file:lines() do
+										table.insert(ws_folders_lsp, line)
+									end
+									file:close()
+								end
+							end
+							for _, line in ipairs(ws_folders_lsp) do
+								vim.lsp.buf.add_workspace_folder(line)
+							end
+						end
+
+						return {
+							on_attach = bemol,
+							cmd = {
+								"jdtls",
+								"--jvm-arg=-javaagent:" .. require("mason-registry")
+									.get_package("jdtls")
+									:get_install_path() .. "/lombok.jar",
+							},
+						}
+					end),
 				},
 			})
 		end,
