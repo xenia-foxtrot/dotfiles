e7b75359ca77c94d91e53640f3f30a6aded792aa
Xenia Foxtrot <me@xenia.sh>
2025-05-04 20:19:46 -0700

    Fix editor plugins

--- private_dot_config/nvim/lua/plugins/editor/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/editor/gitsigns.lua
@@ -1,85 +1,83 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			on_attach = function(bufnr)
-				local gitsigns = require("gitsigns")
-				local utils = require("utils")
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+			local utils = require("utils")
 
-				local km_jump_next = {
-					"n",
-					"]c",
+			local km_jump_next = {
+				"n",
+				"]c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end,
+				{ desc = "Jump to next git [c]hange" },
+			}
+
+			local km_jump_prev = {
+				"n",
+				"[c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end,
+				{ desc = "Jump to previous git [c]hange" },
+			}
+
+			utils.set_keymaps({
+				km_jump_next,
+				km_jump_prev,
+				{
+					"v",
+					"<leader>hs",
 					function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "]c", bang = true })
-						else
-							gitsigns.nav_hunk("next")
-						end
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 					end,
-					{ desc = "Jump to next git [c]hange" },
-				}
-
-				local km_jump_prev = {
-					"n",
-					"[c",
+					{ desc = "git [s]tage hunk" },
+				},
+				{
+					"v",
+					"<leader>hr",
 					function()
-						if vim.wo.diff then
-							vim.cmd.normal({ "[c", bang = true })
-						else
-							gitsigns.nav_hunk("prev")
-						end
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 					end,
-					{ desc = "Jump to previous git [c]hange" },
-				}
-
-				utils.set_keymaps({
-					km_jump_next,
-					km_jump_prev,
-					{
-						"v",
-						"<leader>hs",
-						function()
-							gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-						end,
-						{ desc = "git [s]tage hunk" },
-					},
-					{
-						"v",
-						"<leader>hr",
-						function()
-							gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-						end,
-						{ desc = "git [r]eset hunk" },
-					},
-					{ "n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" } },
-					{ "n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" } },
-					{ "n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" } },
-					{ "n", "<leader>hR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" } },
-					{ "n", "<leader>hp", gitsigns.preview_hunk, { desc = "git [p]review hunk" } },
-					{ "n", "<leader>hb", gitsigns.blame_line, { desc = "git [b]lame line" } },
-					{ "n", "<leader>hd", gitsigns.diffthis, { desc = "git [d]iff against index" } },
-					{
-						"n",
-						"<leader>hD",
-						function()
-							gitsigns.diffthis("@")
-						end,
-						{ desc = "git [D]iff against index" },
-					},
-					{
-						"n",
-						"<leader>tb",
-						gitsigns.toggle_current_line_blame,
-						{ desc = "[T]oggle git show [b]lame line" },
-					},
-					{
-						"n",
-						"<leader>tD",
-						gitsigns.preview_hunk_inline,
-						{ desc = "[T]oggle git show [D]eleted" },
-					},
-				}, { buffer = bufnr })
-			end,
-		},
+					{ desc = "git [r]eset hunk" },
+				},
+				{ "n", "<leader>hs", gitsigns.stage_hunk, { desc = "git [s]tage hunk" } },
+				{ "n", "<leader>hr", gitsigns.reset_hunk, { desc = "git [r]eset hunk" } },
+				{ "n", "<leader>hS", gitsigns.stage_buffer, { desc = "git [S]tage buffer" } },
+				{ "n", "<leader>hR", gitsigns.reset_buffer, { desc = "git [R]eset buffer" } },
+				{ "n", "<leader>hp", gitsigns.preview_hunk, { desc = "git [p]review hunk" } },
+				{ "n", "<leader>hb", gitsigns.blame_line, { desc = "git [b]lame line" } },
+				{ "n", "<leader>hd", gitsigns.diffthis, { desc = "git [d]iff against index" } },
+				{
+					"n",
+					"<leader>hD",
+					function()
+						gitsigns.diffthis("@")
+					end,
+					{ desc = "git [D]iff against index" },
+				},
+				{
+					"n",
+					"<leader>tb",
+					gitsigns.toggle_current_line_blame,
+					{ desc = "[T]oggle git show [b]lame line" },
+				},
+				{
+					"n",
+					"<leader>tD",
+					gitsigns.preview_hunk_inline,
+					{ desc = "[T]oggle git show [D]eleted" },
+				},
+			}, { buffer = bufnr })
+		end,
 	},
 }
--- private_dot_config/nvim/lua/plugins/editor/which-key.lua
+++ private_dot_config/nvim/lua/plugins/editor/which-key.lua
@@ -1,39 +1,37 @@
 ---@module "which-key"
 ---@type LazySpec
+-- Shows pending keybinds with a nice popup
 return {
-	-- Shows pending keybinds with a nice popup
-	{
-		"folke/which-key.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer local keymaps",
-			},
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer local keymaps",
 		},
-		---@type wk.Opts
-		opts = {
-			delay = 0,
-			icons = {
-				-- I always use nerd font, so set mappings to true and use default
-				mappings = true,
-				keys = {},
-			},
-			-- Configure group descriptions
-			-- We'll use these groups in other keybinds
-			spec = {
-				{ "g", group = "[G]oto" },
-				{ "<leader>c", group = "[c]ode", mode = { "n", "x" } },
-				{ "<leader>g", group = "[g]it" },
-				{ "<leader>s", group = "[s]earch" },
-				{ "<leader>t", group = "[t]oggle" },
-				{ "<leader>h", group = "Git [h]unk", mode = { "n", "v" } },
-				{ "<leader>x", group = "diagnostics/quickfi[x]" },
-			},
+	},
+	---@type wk.Opts
+	opts = {
+		delay = 0,
+		icons = {
+			-- I always use nerd font, so set mappings to true and use default
+			mappings = true,
+			keys = {},
+		},
+		-- Configure group descriptions
+		-- We'll use these groups in other keybinds
+		spec = {
+			{ "g", group = "[G]oto" },
+			{ "<leader>c", group = "[c]ode", mode = { "n", "x" } },
+			{ "<leader>g", group = "[g]it" },
+			{ "<leader>s", group = "[s]earch" },
+			{ "<leader>t", group = "[t]oggle" },
+			{ "<leader>h", group = "Git [h]unk", mode = { "n", "v" } },
+			{ "<leader>x", group = "diagnostics/quickfi[x]" },
 		},
 	},
 }
