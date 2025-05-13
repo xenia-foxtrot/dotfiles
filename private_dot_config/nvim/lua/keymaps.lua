ecdc53d9d8b4d6d21d2050db0380d21d222b5dba
Xenia Foxtrot <me@xenia.sh>
2025-05-12 23:30:27 -0700

    Remove default lsp keymaps

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -33,3 +33,9 @@ local keymaps = {
 	{ "<CR>", "o<Esc>" },
 }
 utils.set_keymaps(keymaps)
+
+-- Disable default kepmaps for lsp
+vim.keymap.del("n", "gra")
+vim.keymap.del("n", "grn")
+vim.keymap.del("n", "gri")
+vim.keymap.del("n", "grr")
