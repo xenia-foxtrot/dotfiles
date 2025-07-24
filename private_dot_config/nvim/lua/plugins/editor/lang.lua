1b19286ce407edf816ae6155393ee677fccf8776
Izzy Lancaster <isaavram@amazon.com>
2025-07-24 12:46:52 -0700

    Add markdown support

---
+++ private_dot_config/nvim/lua/plugins/editor/lang.lua
@@ -0,0 +1,7 @@
+--- @module "lazy"
+-- Plugins in this module define language support
+
+return {
+	'MeanderingProgrammer/render-markdown.nvim',
+	dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+}
