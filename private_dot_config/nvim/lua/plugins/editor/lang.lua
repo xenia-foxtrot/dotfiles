f12d4ef39940096d1ede6f54af072fabfcaa7491
Izzy Lancaster <isaavram@amazon.com>
2025-07-24 12:55:43 -0700

    Added rust language support

--- private_dot_config/nvim/lua/plugins/editor/lang.lua
+++ private_dot_config/nvim/lua/plugins/editor/lang.lua
@@ -2,6 +2,13 @@
 -- Plugins in this module define language support
 
 return {
-	'MeanderingProgrammer/render-markdown.nvim',
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+	{
+		'MeanderingProgrammer/render-markdown.nvim',
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+	},
+	{
+		'mrcjkb/rustaceanvim',
+		version = '^6', -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
 }
